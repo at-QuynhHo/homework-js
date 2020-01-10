29cf535803ef24dbf9c0e7f742160b1a1d12790c
Quynh Ho <quynh.ho@asiantech.vn>
2020-01-10 10:24:33 +0700

    update-version-2.0

--- EX1/exercise3.js
+++ EX1/exercise3.js
@@ -1,22 +1,31 @@
-function isDivisibleBy3(str){
+function divisibleBy3(str){
   var arr0 = ['0', '3', '6', '9'];
   var arr1 = ['2', '5', '8'];
   var arr2 = ['1', '4', '7'];
   var result;
   var sum = 0;
   for (var i = 0; i < str.length; i++) {
-    if (str.charAt(i) !== "*") { sum += Number(str.charAt(i)); }
+    if (str.charAt(i) !== "*") { 
+      sum += Number(str.charAt(i)); 
+    }
   }
-  if (sum % 3 === 0) { result = arr0.map((item) => str.replace("*", item));
+  if (sum % 3 === 0) {
+    result = arr0.map(function(item) {
+      return str.replace("*", item);
+    })
   } else if (sum % 3 === 1) {
-    result = arr1.map((item) => str.replace("*", item));
+    result = arr1.map(function(item) {
+      return str.replace("*", item);
+    })
   } else {
-    result = arr2.map((item) => str.replace("*", item));
+    result = arr2.map(function(item) {
+      return str.replace("*", item);
+    }) 
   }
   return result;
 }
 
 
 
-console.log(isDivisibleBy3("1*9"));
-console.log(isDivisibleBy3("1234567890*"));
+console.log(divisibleBy3("1*9"));
+console.log(divisibleBy3("1234567890*")); 
